aaf8be27994c82a89deb7ab4b603edbf31943462
Renae Aurisch <Renae.Aurisch@hotmail.com>
2019-04-11 11:23:47 +0930

    Fixed repo to add in particle effects

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+    public ParticleSystem LeftDust;
+    public ParticleSystem RightDust;
+
+    public void LeftFootstep()
+    {
+        LeftDust.gameObject.SetActive(false);
+        LeftDust.gameObject.SetActive(true);
+    }
+
+    public void RightFootstep()
+    {
+        RightDust.gameObject.SetActive(false);
+        RightDust.gameObject.SetActive(true);
+    }
+
+    public void LeftCrouch()
+    {
+        LeftDust.gameObject.SetActive(false);
+        LeftDust.gameObject.SetActive(true);
+    }
+
+    public void RightCrouch()
+    {
+        RightDust.gameObject.SetActive(false);
+        RightDust.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/MartialArts.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MartialArts : MonoBehaviour {
+
+    Animator animator;
+    public ParticleSystem kickParticles;
+    public ParticleSystem punchParticles;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+            animator.SetTrigger("Kick");
+
+        if (Input.GetKeyDown(KeyCode.P))
+            animator.SetTrigger("Punch");
+    }
+
+    public void kick()
+    {
+        if (kickParticles)
+        {
+            kickParticles.gameObject.SetActive(false);
+            kickParticles.gameObject.SetActive(true);
+        }
+    }
+
+    public void punch()
+    {
+        if (punchParticles)
+        {
+            punchParticles.gameObject.SetActive(false);
+            punchParticles.gameObject.SetActive(true);
+        }
+    }
+}
